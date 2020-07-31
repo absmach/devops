bbd9152216bd041e67d987c9831c9a3b1c525631
Ivan Milošević <iva@blokovi.com>
2020-07-31 23:09:25 +0200

    MF32 - Fix mTLS setup (#53)
    * mTLS certs synchronized with mainflux core
    
    Signed-off-by: Ivan Milošević <iva@blokovi.com>
    
    * remove comments
    
    Signed-off-by: Ivan Milošević <iva@blokovi.com>

--- charts/mainflux/secrets/secrets.sh
+++ charts/mainflux/secrets/secrets.sh
@@ -2,9 +2,9 @@
 # Copyright (c) Mainflux
 # SPDX-License-Identifier: Apache-2.0
 
-kubectl -n mf create secret tls nginx-internal-mtls-tls \
-    --key nginx-internal-mtls.key \
-    --cert nginx-internal-mtls.crt
+kubectl -n mf create secret tls mainflux-server \
+    --key mainflux-server.key \
+    --cert mainflux-server.crt
 
-kubectl -n mf create secret generic nginx-internal-mtls-intermediate-crt \
-    --from-file=intermediate.crt=nginx-internal-mtls-intermediate.crt
+kubectl -n mf create secret generic ca \
+    --from-file=ca.crt
