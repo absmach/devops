beba5e00bbb135ae9618884e58d3f216e22963fd
JeffMboya <jangina.mboya@gmail.com>
2024-11-18 09:31:05 +0300

    Remove docker/addons/vault directory
    Signed-off-by: JeffMboya <jangina.mboya@gmail.com>

--- docker/addons/vault/readers/timescale/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Abstract Machines
-// SPDX-License-Identifier: Apache-2.0
-
-// Package timescale contains repository implementations using Timescale as
-// the underlying database.
-package timescale
